f5b1b1486bd5413d6fbc3e64a26bf075416704ae
Sarah <sarah@sarahfrench.co.uk>
2019-02-04 09:50:40 +0000

    Trying to get URL requests working - incomplete

--- consoleRunner.js
+++ consoleRunner.js
@@ -13,11 +13,16 @@ const TFL_BASE_URL = 'https://api.tfl.gov.uk';
 export default class ConsoleRunner {
 
     promptForPostcode() {
-      return new Promise(function(resolve) {
-        readline.question('\nEnter your postcode: ', (postcode) => {
-          resolve(postcode);
-        });
-      });
+        try {
+          return new Promise(function(resolve) {
+            readline.question('\nEnter your postcode: ', (postcode) => {
+              resolve(postcode);
+            });
+          });
+        } catch (error){
+          console.log("Issue in promptForPostcode");
+          console.log(error)
+        }
     }
 
     displayStopPoints(stopPoints) {
@@ -28,35 +33,53 @@ export default class ConsoleRunner {
 
     buildUrl(url, endpoint, parameters) {
         const requestUrl = new URL(endpoint, url);
+        console.log(requestUrl);
         parameters.forEach(param => requestUrl.searchParams.append(param.name, param.value));
         return requestUrl.href;
     }
 
-    makeGetRequest(baseUrl, endpoint, parameters, callback) {
-        const url = this.buildUrl(baseUrl, endpoint, parameters);
+    // makeGetRequest(baseUrl, endpoint, parameters, callback) {
+    //     try {
+    //       const url = this.buildUrl(baseUrl, endpoint, parameters);
+    //       return new Promise((resolve, reject) => {
+    //         try {
+    //           request.get(url, (err, response, body) => {
+    //             if (response.statusCode !== 200) {
+    //               console.log(response.statusCode);
+    //               throw new Error("Error thrown due to status code");
+    //             } else {
+    //               resolve(callback(body));
+    //             } catch (err){
+    //               reject(err);
+    //             }
+    //           })
+    //               })}
+    //         } catch (error) {
+    //           console.log("Issue in makeGetRequest");
+    //           console.log(error)
+    //         }
 
-        request.get(url, (err, response, body) => {
-            if (err) {
-                console.log(err);
-            } else if (response.statusCode !== 200) {
-                console.log(response.statusCode);
-            } else {
-                callback(body);
+
+    getLocationForPostCode(postcode) {
+          try {
+            const locationResults = new Promise ((resolve, reject) => {
+              this.makeGetRequest(POSTCODES_BASE_URL, `postcodes/${postcode}`, [], responseBody => {
+                const jsonBody = JSON.parse(responseBody);
+                resolve({ latitude: jsonBody.result.latitude, longitude: jsonBody.result.longitude });
+                reject(console.log("Issue with getLocationForPostCode"))
+              })
+            })
+            return Promise.all([postcode, locationResults])
+          } catch (error){
+            console.log("Issue in getLocationForPostCode");
+            console.log(error)
             }
-        });
-    }
-
-    getLocationForPostCode(postcode, callback) {
-        this.makeGetRequest(POSTCODES_BASE_URL, `postcodes/${postcode}`, [], function(responseBody) {
-            const jsonBody = JSON.parse(responseBody);
-            callback({ latitude: jsonBody.result.latitude, longitude: jsonBody.result.longitude });
-        });
     }
 
     getNearestStopPoints(latitude, longitude, count, callback) {
         this.makeGetRequest(
             TFL_BASE_URL,
-            `StopPoint`, 
+            `StopPoint`,
             [
                 {name: 'stopTypes', value: 'NaptanPublicBusCoachTram'},
                 {name: 'lat', value: latitude},
@@ -66,7 +89,7 @@ export default class ConsoleRunner {
                 {name: 'app_key', value: '' /* Enter your app key here */}
             ],
             function(responseBody) {
-                const stopPoints = JSON.parse(responseBody).stopPoints.map(function(entity) { 
+                const stopPoints = JSON.parse(responseBody).stopPoints.map(function(entity) {
                     return { naptanId: entity.naptanId, commonName: entity.commonName };
                 }).slice(0, count);
                 callback(stopPoints);
@@ -75,12 +98,18 @@ export default class ConsoleRunner {
     }
 
     run() {
-        const that = this;
+        let that = this
         that.promptForPostcode()
           .then(postcode => {
             readline.close();
             postcode = postcode.replace(/\s/g, '');
             console.log(`\nFinding busstops near ${postcode}`)
+          })
+          .then(postcode => {
+            that.getLocationForPostCode(postcode)
+          })
+          .then(values => {
+            console.log(values)
           })
     }
 }
